7b1b9e81970387484c5e81752b291c8e6a78c3f8
Mario Paone <paonemario@yahoo.it>
2023-09-13 18:57:50 +0200

    fixed menu issue

--- src/components/DropDownProjects.js
+++ src/components/DropDownProjects.js
@@ -10,21 +10,18 @@ function DropDownProjects() {
                         transition-all duration-300 ease-liner "
       >
         <ul className="px-2 py-1 font-bold font-sans">
-          <li className="px-1 rounded-xl hover:bg-[#7bdff2] ">
-            <Link to="project1" spy={true} smooth={true} duration={500}>
-              Portfolio
-            </Link>
-          </li>
-          <li className="px-1 rounded-xl hover:bg-[#7bdff2] ">
-            <Link to="project2" spy={true} smooth={true} duration={500}>
+          <Link to="project1" spy={true} smooth={true} duration={500}>
+            <li className="px-1 rounded-xl hover:bg-[#7bdff2] ">Portfolio</li>
+          </Link>
+
+          <Link to="project2" spy={true} smooth={true} duration={500}>
+            <li className="px-1 rounded-xl hover:bg-[#7bdff2] ">
               Madetteranean tour
-            </Link>
-          </li>
-          <li className="px-1 rounded-xl hover:bg-[#7bdff2] ">
-            <Link to="project3" spy={true} smooth={true} duration={500}>
-              Berlusca
-            </Link>
-          </li>
+            </li>
+          </Link>
+          <Link to="project3" spy={true} smooth={true} duration={500}>
+            <li className="px-1 rounded-xl hover:bg-[#7bdff2] ">Berlusca</li>
+          </Link>
         </ul>
       </div>
     </div>
