c83643ed9ffe0758abd5404199c54529408ea350
Mario Paone <paonemario@yahoo.it>
2023-09-16 14:59:06 +0200

    added first carousel

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -1,8 +1,25 @@
+import Carousel from "./Carousel";
+
 function Project1() {
+  const slides = [
+    {
+      src: "https://picsum.photos/seed/img1/600/400",
+      alt: "Image 1 for carousel",
+    },
+    {
+      src: "https://picsum.photos/seed/img2/600/400",
+      alt: "Image 2 for carousel",
+    },
+    {
+      src: "https://picsum.photos/seed/img3/600/400",
+      alt: "Image 3 for carousel",
+    },
+  ];
+
   return (
     <div className="h-screen grid place-items-center" id="project1">
-      <div className="grid place-items-center">
-        <div className="font-bold text-5xl font-mono">Project 1</div>
+      <div className="px-16">
+        <Carousel data={slides} />
       </div>
     </div>
   );
